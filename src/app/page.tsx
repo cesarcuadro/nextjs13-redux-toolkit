0ceb19882d9cce6bb0d17810f49b847c0ed6ae68
cesarcuadro <cesarcuadro777@gmail.com>
2023-05-23 20:09:04 -0700

    decrement&reset button functionality works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,13 @@ export default function Home() {
   const count = useAppSelector((state) => state.counterReducer.value);
   const dispatch = useAppDispatch() as Dispatch<AnyAction>;
 
-  const handleIncrement = () => {
-    dispatch(increment());
-  };
-
   return (
     <main>
       <div>
         <h4>{count}</h4>
-        <button onClick={handleIncrement}>increment</button>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        <button onClick={() =>dispatch(reset())}>reset</button>
       </div>
     </main>
   );
